c7072c6d70a29e163c0e8779203f1ad9deb3117f
Owen Henry <owen.henry@duke.edu>
2021-09-17 13:13:44 -0400

    9-17 done

--- 9-17.R
+++ 9-17.R
@@ -39,8 +39,37 @@ ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g, color = species)) +
   labs(title = "Plot 4")
 
 
+library(maps)
+ggplot(map_data("state"), aes(long, lat, group = group)) +
+  geom_polygon(fill = "white", color = "#3c3b6e") +
+  labs(x = NULL, y = NULL) +
+  coord_quickmap()
 
+ggplot(map_data("state"), aes(long, lat, group = group)) +
+  geom_polygon(fill = "white", color = "#3c3b6e") +
+  labs(x = NULL, y = NULL) +
+  coord_map()
 
+ggplot(map_data("state"), aes(long, lat, group = group)) +
+  geom_polygon(fill = "white", color = "#3c3b6e") +
+  labs(x = NULL, y = NULL) +
+  coord_map(projection = "stereographic")
+
+ggplot(map_data("world"), aes(long, lat, group = group)) +
+  geom_polygon(fill = "white", color = "#3c3b6e") +
+  labs(x = NULL, y = NULL) +
+  coord_map(projection = "mercator")
+
+ggplot(penguins, aes(y = species, x = body_mass_g, fill = species)) +
+  geom_boxplot(show.legend = FALSE) +
+  facet_grid(island ~ ., scales = "free_y", space = "free") +
+  labs(title = "Free y scale and spacing")
+
+penguins_sans_species <- penguins %>% select(-species)
+ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g)) +
+  geom_point(data = penguins_sans_species, color = "gray") +
+  geom_point(aes(color = species)) +
+  facet_wrap(~species)
 
 
 
